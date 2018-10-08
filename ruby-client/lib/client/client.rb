eb937604821762a1fb7e1a43e966830db9b51c5a
anaritamarques <anarrocham@gmail.com>
2018-10-08 18:16:40 +0100

    ruby-client content finally added

---
+++ ruby-client/lib/client/client.rb
@@ -0,0 +1,36 @@
+require 'graphlient'
+require 'pry-nav'
+
+client = Graphlient::Client.new('http://0.0.0.0:4000/graphql', ##change this url
+  headers: {
+  }
+)
+client.schema
+
+AuthorsQuery = client.parse do
+  query do
+    authors do
+      id
+      name
+      posts do
+        title
+        content
+      end
+    end
+  end
+end
+
+result = client.execute(AuthorsQuery)
+
+i = 0
+j = 0
+while i < result.data.authors.count
+  puts result.data.authors[i].name
+  j = 0
+  while j < result.data.authors[i].posts.count
+    puts result.data.authors[i].posts[j].title
+    puts result.data.authors[i].posts[j].content
+    j+=1
+  end
+  i += 1
+end
